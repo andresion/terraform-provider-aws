459f4afca1f5b0a50292d654834737fcf7fded05
Jack Bruno <jzbruno@gmail.com>
2019-06-12 22:01:58 -0600

    Update to address review comments, move common code into resource of first use

--- aws/resource_aws_lex_slot_type.go
+++ aws/resource_aws_lex_slot_type.go
@@ -12,6 +12,48 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
+// Many of the Lex resources require complex nested objects. Terraform maps only support simple key
+// value pairs and not complex or mixed types. That is why these resources are defined using the
+// schema.TypeList and a max of 1 item instead of the schema.TypeMap.
+
+func expandLexSet(s *schema.Set) (items []map[string]interface{}) {
+	for _, rawItem := range s.List() {
+		item, ok := rawItem.(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		items = append(items, item)
+	}
+
+	return
+}
+
+func flattenLexEnumerationValues(values []*lexmodelbuildingservice.EnumerationValue) (flattened []map[string]interface{}) {
+	for _, value := range values {
+		flattened = append(flattened, map[string]interface{}{
+			"synonyms": flattenStringList(value.Synonyms),
+			"value":    aws.StringValue(value.Value),
+		})
+	}
+
+	return
+}
+
+// Expects a slice of maps representing the Lex objects.
+// The value passed into this function should have been run through the expandLexSet function.
+// Example: []map[value: lilies synonyms:[]lirium]]
+func expandLexEnumerationValues(rawValues []map[string]interface{}) (enums []*lexmodelbuildingservice.EnumerationValue) {
+	for _, rawValue := range rawValues {
+		enums = append(enums, &lexmodelbuildingservice.EnumerationValue{
+			Synonyms: expandStringList(rawValue["synonyms"].([]interface{})),
+			Value:    aws.String(rawValue["value"].(string)),
+		})
+	}
+
+	return
+}
+
 func resourceAwsLexSlotType() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceAwsLexSlotTypeCreate,
